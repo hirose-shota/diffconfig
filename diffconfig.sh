96307a784b64cff534d4d91bfd94c90197f9cd59
shota hirose <muchhappiness83@gmail.com>
2020-10-03 06:28:15 +0000

    ディレクトリの除外が可能になる

--- diffconfig.sh
+++ diffconfig.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-configdir="/home/vagrant/diffconfig/tftpboot/"
+currentdir=`pwd`
+source ${currentdir}/config.sh
 
-config1=$(find $configdir -type f | peco --prompt="config1>")
-config2=$(find $configdir -type f | peco --prompt="config2>")
+if [ -z "${ignoredir}" ]; then
+  config1=$(find $configdir -type f -print | sort -r | peco --prompt="config1>")
+  config2=$(find $configdir -type f -print | sort -r | peco --prompt="config2>")
+else
+  config1=$(find $configdir -type d -name ${ignoredir} -prune -o -type f -print | sort -r | peco --prompt="config1>")
+  config2=$(find $configdir -type d -name ${ignoredir} -prune -o -type f -print | sort -r | peco --prompt="config2>")
+fi
 
 echo "diff $config1 $config2"
 diff "$config1" "$config2"
