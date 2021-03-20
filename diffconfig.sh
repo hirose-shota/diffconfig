044a56a764cefe887e7afa041c359253f80da8a3
hirose-shota <muchhappiness83@gmail.com>
2021-03-20 14:48:17 +0000

    shellcheckを使用しての修正
    sourceコマンドの前にshellcheckのコメントを追加
    変数にダブルクォーテーションをつける

--- diffconfig.sh
+++ diffconfig.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# shellcheck source=./config.sh
 source ./config.sh
 
 if [ -n "${1}" ]; then
@@ -7,11 +8,11 @@ if [ -n "${1}" ]; then
 fi
 
 if [ -z "${ignoredir}" ]; then
-  config1=$(find ${configdir} -type f -print | sort -r | peco --prompt="config1>")
-  config2=$(find ${configdir} -type f -print | sort -r | peco --prompt="config2>")
+  config1=$(find "${configdir}" -type f -print | sort -r | peco --prompt="config1>")
+  config2=$(find "${configdir}" -type f -print | sort -r | peco --prompt="config2>")
 else
-  config1=$(find ${configdir} -type d -name ${ignoredir} -prune -o -type f -print | sort -r | peco --prompt="config1>")
-  config2=$(find ${configdir} -type d -name ${ignoredir} -prune -o -type f -print | sort -r | peco --prompt="config2>")
+  config1=$(find "${configdir}" -type d -name "${ignoredir}" -prune -o -type f -print | sort -r | peco --prompt="config1>")
+  config2=$(find "${configdir}" -type d -name "${ignoredir}" -prune -o -type f -print | sort -r | peco --prompt="config2>")
 fi
 
 echo "diff -u ${config1} ${config2}"
