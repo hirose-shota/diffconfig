4769702ab23a2b55330b8c1ca6d52623a1effa4b
hirose-shota <muchhappiness83@gmail.com>
2021-03-20 14:11:19 +0000

    変数の書き方の統一

--- diffconfig.sh
+++ diffconfig.sh
@@ -3,17 +3,17 @@
 currentdir=`pwd`
 source ${currentdir}/config.sh
 
-if [ -n "$1" ]; then
-  configdir+="/$1"
+if [ -n "${1}" ]; then
+  configdir+="/${1}"
 fi
 
 if [ -z "${ignoredir}" ]; then
-  config1=$(find $configdir -type f -print | sort -r | peco --prompt="config1>")
-  config2=$(find $configdir -type f -print | sort -r | peco --prompt="config2>")
+  config1=$(find ${configdir} -type f -print | sort -r | peco --prompt="config1>")
+  config2=$(find ${configdir} -type f -print | sort -r | peco --prompt="config2>")
 else
-  config1=$(find $configdir -type d -name ${ignoredir} -prune -o -type f -print | sort -r | peco --prompt="config1>")
-  config2=$(find $configdir -type d -name ${ignoredir} -prune -o -type f -print | sort -r | peco --prompt="config2>")
+  config1=$(find ${configdir} -type d -name ${ignoredir} -prune -o -type f -print | sort -r | peco --prompt="config1>")
+  config2=$(find ${configdir} -type d -name ${ignoredir} -prune -o -type f -print | sort -r | peco --prompt="config2>")
 fi
 
-echo "diff -u $config1 $config2"
-diff -u "$config1" "$config2"
+echo "diff -u ${config1} ${config2}"
+diff -u "${config1}" "${config2}"
